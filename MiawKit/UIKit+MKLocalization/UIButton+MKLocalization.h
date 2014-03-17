b1a795ea847a72d76dc876d8b8070eda46131f61
Kristian Andersen <me@kristianandersen.me>
2014-03-17 11:42:38 +0100

    Added public header documentation for UIKit extensions

--- MiawKit/UIKit+MKLocalization/UIButton+MKLocalization.h
+++ MiawKit/UIKit+MKLocalization/UIButton+MKLocalization.h
@@ -10,15 +10,43 @@
 
 @interface UIButton (MKLocalization)
 
+/*!
+ * Localize the button using the current text of the button
+ * as the key value for the localized string lookup.
+ */
 - (void)mk_localize;
 
+/*!
+ * Localize the button using the given localization key
+ * @param localization The localization key
+ */
 - (void)mk_localize:(NSString *)localization;
 
+/*!
+ * Localize the button using the given localization key
+ * and format it with the given extra parameters.
+ * @param localization The localization key
+ * @param ... The extra parameters used to format the localized string
+ */
 - (void)mk_localizeWithFormat:(NSString *)localization, ...;
 
+/*!
+ * Localize the button using the given localization key
+ * from the given table.
+ * @param localization The localization key
+ * @param table The table used for the localized string lookup
+ */
 - (void)mk_localize:(NSString *)localization
           fromTable:(NSString *)table;
 
+/*!
+ * Localize the button using the given localization key
+ * from the given table and format it using the given
+ * extra parameters.
+ * @param localization The localization key
+ * @param table The table used for the localized string lookup
+ * @param ... The extra parameters used to format the localized string
+ */
 - (void)mk_localizeFromTable:(NSString *)table
                   withFormat:(NSString *)localization, ...;
 
