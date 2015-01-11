b5bb6c43da36d35859ec9197813ce1e73ffed64e
Kristian Andersen <hello@kristian.co>
2015-01-11 13:37:15 +0100

    Added xcode project with framework target

---
+++ MiawKit/MiawKit.h
@@ -0,0 +1,23 @@
+//
+//  MiawKit.h
+//  MiawKit
+//
+//  Created by Kristian Andersen on 11/01/15.
+//  Copyright (c) 2015 Robocat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MiawKit.
+FOUNDATION_EXPORT double MiawKitVersionNumber;
+
+//! Project version string for MiawKit.
+FOUNDATION_EXPORT const unsigned char MiawKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiawKit/PublicHeader.h>
+
+#import <MiawKit/MKFunctions.h>
+#import <MiawKit/MKLocalization.h>
+#import <MiawKit/NSString+MKLocalization.h>
+#import <MiawKit/UIButton+MKLocalization.h>
+#import <MiawKit/UILabel+MKLocalization.h>
